5f60780a51d4ff785add508a7ba7c47e2ba85c23
Crynner <calvin.shih19@gmail.com>
2025-04-04 10:47:33 +1300

    initial commit

---
+++ src/main/java/com/crynner/App.java
@@ -0,0 +1,13 @@
+package com.crynner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/crynner/BankAccount.java
@@ -0,0 +1,27 @@
+package com.crynner;
+
+public class BankAccount {
+    // Add necessary fields here
+
+    public BankAccount() {
+        // Constructor implementation
+    }
+
+    public void deposit(double amount) {
+        // Implement this method
+    }
+
+    public void withdraw(double amount) {
+        // Implement this method
+    }
+
+    public void transferTo(BankAccount other, double amount) {
+        // Implement this method
+    }
+
+    public static int getTotalAccounts() {
+        // Implement this method
+        return 0;
+    }
+}
+
