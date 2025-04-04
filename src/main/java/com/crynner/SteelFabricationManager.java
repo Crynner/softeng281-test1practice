589563a73029385cbf599108c0454af317d4010f
Crynner <calvin.shih19@gmail.com>
2025-04-04 20:45:12 +1300

    completed q1: steel fabrication

--- src/main/java/com/crynner/SteelFabricationManager.java
+++ src/main/java/com/crynner/SteelFabricationManager.java
@@ -3,44 +3,66 @@ package com.crynner;
 import java.util.ArrayList;
 
 public class SteelFabricationManager {
-    // Add fields as required
+    ArrayList<FabricationProject> projects = new ArrayList<>();
 
     public SteelFabricationManager() {
     }
 
     public void addProject(String name, double cost) {
+        for (FabricationProject project : projects) {
+            if (project.getName().equals(name)) {
+                project.setCost(cost);
+                return;
+            }
+        }
+        projects.add(new FabricationProject(name, cost));
     }
 
     public double getTotalCost() {
-        return 0.0;
+        double totalCost = 0;
+        for (FabricationProject project : projects) {
+            totalCost += project.getCost();
+        }
+        return totalCost;
     }
 
     public double getProjectCost(String name) {
-        return 0.0;
+        for (FabricationProject project : projects) {
+            if (project.getName().equals(name)) {
+                return project.getCost();
+            }
+        }
+        return -1;
     }
 
     public int getProjectCount() {
-        return 0;
+        return projects.size();
     }
 }
 
 class FabricationProject {
-    // Add fields as required
+    String name;
+    double cost;
+
     public FabricationProject(String name, double cost) {
+        this.name = name;
+        this.cost = cost;
     }
 
     public String getName() {
-        return "";
+        return name;
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public double getCost() {
-        return 0.0;
+        return cost;
     }
 
     public void setCost(double cost) {
+        this.cost = cost;
     }
 }
 
