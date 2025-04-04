204c5b243ce615fb9b52f15857e1f460f09484ce
Crynner <calvin.shih19@gmail.com>
2025-04-04 20:29:34 +1300

    completed q2: disaster event

--- src/main/java/com/crynner/DisasterEvent.java
+++ src/main/java/com/crynner/DisasterEvent.java
@@ -1,42 +1,63 @@
 package com.crynner;
 
 public class DisasterEvent {
-    // Add necessary fields here
+    String id;
+    String name;
+    String location;
+    String impactLevel;
+    double damageCost;
 
     public DisasterEvent(String id, String name) {
+        this.id = id;
+        this.name = name;
+        location = "Unknown";
+        impactLevel = "Unknown";
+        damageCost = 0;
     }
 
     public DisasterEvent(String id, String name, String location, String impactLevel, double damageCost) {
+        this.id = id;
+        this.name = name;
+        this.location = location;
+        this.impactLevel = impactLevel;
+        this.damageCost = damageCost;
     }
 
     public DisasterEvent(String id, String name, String location) {
+        this.id = id;
+        this.name = name;
+        this.location = location;
+        impactLevel = "Unknown";
+        damageCost = 0;
     }
 
     public String getDisasterDetails() {
-        return "";
+        return String.format("ID: %s, Name: %s, Location: %s, Impact Level: %s, Damage Cost: %.1f",
+            id, name, location, impactLevel, damageCost);
     }
 
     public String getId() {
-        return "";
+        return id;
     }
 
     public String getName() {
-        return "";
+        return name;
     }
 
     public String getLocation() {
-        return "";
+        return location;
     }
 
     public String getImpactLevel() {
-        return "";
+        return impactLevel;
     }
 
     public double getDamageCost() {
-        return 0.0;
+        return damageCost;
     }
 
     public void updateDamageCost(double newDamageCost) {
+        damageCost = newDamageCost;
     }
 }
 
