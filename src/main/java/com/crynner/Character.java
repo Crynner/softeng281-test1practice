582fded1aea08e4faefbab799e539338d683224c
Crynner <calvin.shih19@gmail.com>
2025-04-04 19:43:02 +1300

    completed q4: rpg characters

--- src/main/java/com/crynner/Character.java
+++ src/main/java/com/crynner/Character.java
@@ -1,34 +1,46 @@
 package com.crynner;
 
 public abstract class Character {
+    String name;
+    int level;
 
     public Character(String name, int level) {
+        this.name = name;
+        this.level = level;
     }
 
     public abstract String attack();
 
     public String getDetails() {
-        return "Name: " + null + ", Level: " + null;
+        return "Name: " + name + ", Level: " + level;
     }
 }
 
 class Warrior extends Character {
+    String weaponType;
 
-    public Warrior(String name, int level, String weaponType) {}
+    public Warrior(String name, int level, String weaponType) {
+        super(name, level);
+        this.weaponType = weaponType;
+    }
 
     @Override
     public String attack() {
-        return "";
+        return String.format("%s swings his %s!", name, weaponType);
     }
 }
 
 class Mage extends Character {
+    String element;
 
-    public Mage(String name, int level, String element) {}
+    public Mage(String name, int level, String element) {
+        super(name, level);
+        this.element = element;
+    }
 
     @Override
     public String attack() {
-        return "";
+        return String.format("%s casts a %s spell!", name, element);
     }
 }
 
