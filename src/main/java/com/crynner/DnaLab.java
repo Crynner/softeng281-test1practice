c15fc2a7c42715b3a6b788142c3bd488a86e85e7
Crynner <calvin.shih19@gmail.com>
2025-04-08 22:21:26 +1200

    finished versions of q14 and q15

--- src/main/java/com/crynner/DnaLab.java
+++ src/main/java/com/crynner/DnaLab.java
@@ -1,82 +1,157 @@
 package com.crynner;
 
+import java.util.List;
 import java.util.ArrayList;
 
 class Nucleotide {
+    public static String validBases = "ACTG";
+    private char base;
 
     public Nucleotide(char base) {
+        if (validBases.indexOf(base) == -1) {
+            this.base = 'A';
+            return;
+        }
+        this.base = base;
     }
 
     public char getBase() {
-        return ' ';
+        return base;
     }
 
     public String getComplement() {
-        return "";
+        switch (base){
+        case 'A':
+            return "T";
+        case 'T':
+            return "A";
+        case 'C':
+            return "G";
+        case 'G':
+            return "C";
+        default: // base is invalid - impossible case
+            return "\0";
+        }
+        
     }
 }
 
 class Gene {
+    private List<Nucleotide> bases = new ArrayList<>();
 
     public Gene(String sequence) {
+        for (char c : sequence.toCharArray()) {
+            if (Nucleotide.validBases.indexOf(c) != -1) { // valid base
+                bases.add(new Nucleotide(c));
+            }
+        }
     }
 
     public String getSequence() {
-        return "";
+        String sequence = "";
+        for (Nucleotide n : bases) {
+            sequence += n.getBase();
+        }
+        return sequence;
     }
 
     public String getComplementarySequence() {
-        return "";
+        String sequence = "";
+        for (Nucleotide n : bases) {
+            sequence += n.getComplement();
+        }
+        return sequence;
     }
 
     public boolean containsPattern(String pattern) {
-        return false;
+        return getSequence().contains(pattern);
     }
 
     public int countOccurrences(String subsequence) {
-        return -1;
+        String mainString = getSequence();
+        int index = 0;
+        int counter = 0;
+        while (index != -1) {
+            index = mainString.indexOf(subsequence, index);
+            if (index != -1) { // if match found
+                counter++;
+                index += subsequence.length();
+            }
+        }
+        return counter;
     }
 }
 
 class Organism {
+    private String name;
+    private List<Gene> genes = new ArrayList<>();
 
     public Organism(String name) {
+        this.name = name;
     }
 
     public void addGene(Gene gene) {
+        genes.add(gene);
+    }
+
+    public List<Gene> getGenes() {
+        return genes;
     }
 
     public String getFullDNA() {
-        return "";
+        String totalStr = "";
+        for (Gene gene : genes) {
+            totalStr += gene.getSequence();
+        }
+        return totalStr;
     }
 
     public boolean hasMatchingGene(Organism other) {
+        for (Gene gene : genes) {
+            for (Gene otherGene : other.getGenes()) {
+                if (gene.getSequence().equals(otherGene.getSequence())) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
     public String getName() {
-        return "";
+        return name;
     }
 
     public int getGeneCount() {
-        return -1;
+        return genes.size();
     }
 }
 
 class DnaLab {
-    private ArrayList<Organism> organisms;
+    private List<Organism> organisms = new ArrayList<>();
 
-    public DnaLab() {
-    }
+    public DnaLab() {}
 
     public void registerOrganism(Organism org) {
+        organisms.add(org);
     }
 
     public ArrayList<String> getOrganismsWithGenePattern(String pattern) {
-        return new ArrayList<>();
+        ArrayList<String> matching = new ArrayList<>();
+        for (Organism org : organisms) {
+            for (Gene gene : org.getGenes()) {
+                if (gene.getSequence().contains(pattern)) {
+                    matching.add(org.getName());
+                }
+            }
+        }
+        return matching;
     }
 
     public String generateReport() {
-        return "";
+        String joinedTotal = "";
+        for (Organism org : organisms) {
+            joinedTotal += String.format("Organism: %s\nGene Count: %d\n", org.getName(), org.getGeneCount());
+        }
+        return joinedTotal;
     }
 }
--- src/main/java/com/crynner/Phoneme.java
+++ src/main/java/com/crynner/Phoneme.java
@@ -1,61 +1,112 @@
 package com.crynner;
 
+import java.util.List;
 import java.util.ArrayList;
 
 class Phoneme {
+    private char symbol;
+    private long duration;
+    private double intensity;
+    private boolean vowel;
 
     public Phoneme(char symbol, long durationMs, double intensity, boolean isVowel) {
+        this.symbol = symbol;
+        this.duration = durationMs;
+        this.intensity = intensity;
+        this.vowel = isVowel;
     }
 
     public char getSymbol() {
-        return '\0';
+        return symbol;
     }
 
     public long getDuration() {
-        return -1;
+        return duration;
     }
 
     public double getIntensity() {
-        return -1.0;
+        return intensity;
     }
 
     public boolean isVowel() {
-        return false;
+        return vowel;
     }
 
     public boolean isLong() {
-        return false;
+        return duration > 250;
     }
 
     public void amplify(double factor) {
+        intensity *= factor;
+        if (intensity > 1.0) {
+            intensity = 1.0;
+        } else if (intensity < 0.0) {
+            intensity = 0.0;
+        }
     }
 }
 
 class VowelDetector {
-
-    public VowelDetector() {
-    }
+    List<Phoneme> sounds = new ArrayList<>();
+    List<Phoneme> vowels = new ArrayList<>();
 
     public void addPhoneme(Phoneme p) {
+        sounds.add(p);
+        if (p.isVowel()) {
+            vowels.add(p);
+        }
     }
 
     public int getVowelCount() {
-        return -1;
+        return vowels.size();
     }
 
     public double getAverageIntensity() {
-        return -1.0;
+        double totalIntensity = 0;
+        for (Phoneme sound : sounds) {
+            totalIntensity += sound.getIntensity();
+        }
+        if (sounds.size() != 0) {
+            return totalIntensity / sounds.size();
+        }
+        return totalIntensity;
     }
 
     public char getLongestVowel() {
-        return '\0';
+        Phoneme longestVowel = null;
+        for (Phoneme vowel : vowels) {
+            if (longestVowel == null) { // for first iteration, there is no comparison
+                longestVowel = vowel;
+                continue;
+            }
+            if (vowel.getDuration() > longestVowel.getDuration()) {
+                longestVowel = vowel;
+            }
+        }
+        return longestVowel == null ? '-' : longestVowel.getSymbol();
     }
 
     public void removeShortSounds(long minDuration) {
+        List<Phoneme> toRemove = new ArrayList<>();
+        for (Phoneme sound : sounds) {
+            if (sound.getDuration() < minDuration) {
+                toRemove.add(sound);
+            }
+        }
+        // deleting later to prevent ConcurrentModificationException
+        for (Phoneme removal : toRemove) {
+            sounds.remove(removal);
+            vowels.remove(removal);
+        }
     }
 
     @Override
     public String toString() {
-        return "";
+        String totalString = "";
+        for (Phoneme sound : sounds) {
+            totalString += String.format("Symbol: %s, Duration: %dms, Intensity: %.2f\n",
+                sound.getSymbol(), sound.getDuration(), sound.getIntensity());
+        }
+        return totalString;
     }
 }
